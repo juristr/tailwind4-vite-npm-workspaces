639ed58210e828d318abe0ffa3e7a9e2f62cecf1
Juri <juri.strumpflohner@gmail.com>
2025-02-19 10:55:15 +0100

    add product list filter

--- packages/products/feat-product-list/src/lib/feat-product-list.tsx
+++ packages/products/feat-product-list/src/lib/feat-product-list.tsx
@@ -1,6 +1,7 @@
 import styles from './feat-product-list.module.css';
 import { dataAccessProducts } from '@aishop/data-access-products';
 import { UiProductDetail } from '@aishop/ui-product-detail';
+import { ProductListFilter } from '@aishop/product-list-filter';
 
 export function FeatProductList() {
   return (
@@ -8,6 +9,7 @@ export function FeatProductList() {
       <h1>Welcome to FeatProductList!</h1>
       <p>Data access value: {dataAccessProducts()}</p>
       <UiProductDetail />
+      <ProductListFilter />
     </div>
   );
 }
