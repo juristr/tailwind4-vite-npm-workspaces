deeeb7e8cebf50d87ff78e4ec6161226ba576040
Juri <juri.strumpflohner@gmail.com>
2025-03-04 23:11:35 +0100

    testing

---
+++ packages/src/lib/packages.tsx
@@ -0,0 +1,11 @@
+import styles from './packages.module.css';
+
+export function Packages() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Packages!</h1>
+    </div>
+  );
+}
+
+export default Packages;
