bf26b80a3de83c09621ac996dfa748fa66734d8b
Juri <juri.strumpflohner@gmail.com>
2025-06-05 17:25:50 +0200

    ..

---
+++ packages/orders/feat-orders/README.md
@@ -0,0 +1,7 @@
+# feat-orders
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test feat-orders` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ packages/orders/feat-orders/src/lib/feat-orders.tsx
@@ -0,0 +1,11 @@
+import styles from './feat-orders.module.css';
+
+export function FeatOrders() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to FeatOrders!</h1>
+    </div>
+  );
+}
+
+export default FeatOrders;
